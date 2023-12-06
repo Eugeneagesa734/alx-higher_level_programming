f5787797a66df04947c486b86dc9b13fbae15978
Eugene Agesa <eugeneagesa734@gmail.com>
2023-12-06 00:53:33 -0800

    updated 5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-if __name___ == "__main__":
-    """value of variable a from variable_load_5 is printed"""
+
+if __name__ == "__main__":
+    """value of variable from variable_load_5 is printed."""
     from variable_load_5 import a
-    print("{:d}".format(a))
+
+    print(a)
 
